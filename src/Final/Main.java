4ee9b8dd88d36116aba14c2ba17691152a457abf
Xintong Ruan <tobyruan@Xintongs-MacBook-Pro.local>
2025-05-01 00:11:13 -0400

    second commit

--- src/Final/Main.java
+++ src/Final/Main.java
@@ -3,5 +3,6 @@ package Final;
 public class Main {
 	public static void main(String[] args) {
 		System.out.print("hello world");
+		System.out.print("omg I am finally done with git set up.");
 	}
 }
